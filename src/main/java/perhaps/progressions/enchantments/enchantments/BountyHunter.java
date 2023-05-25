5a0955e5cfc41d4c2b0bfc177c4756e33d45eaeb
mason26611 <masong1508@gmail.com>
2023-05-24 21:17:50 -0400

    Add bounty hunter file

---
+++ src/main/java/perhaps/progressions/enchantments/enchantments/BountyHunter.java
@@ -0,0 +1,34 @@
+package perhaps.progressions.enchantments.enchantments;
+
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.event.entity.living.LivingExperienceDropEvent;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import perhaps.progressions.MythicProgressions;
+import perhaps.progressions.enchantments.EnchantmentRarity;
+import perhaps.progressions.enchantments.Enchantments;
+
+@Mod.EventBusSubscriber(modid = MythicProgressions.MOD_ID)
+public class BountyHunter extends Enchantment {
+    public BountyHunter(Rarity rarity, EnchantmentCategory enchantmentCategory, EquipmentSlot[] equipmentSlots) {
+        super(rarity, enchantmentCategory, equipmentSlots);
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return 3;
+    }
+
+    @Override
+    public int getMinCost(int level) { return EnchantmentRarity.RARE.getMinCost(level); }
+    @Override
+    public int getMaxCost(int level) { return EnchantmentRarity.RARE.getMaxCost(level); }
+}
