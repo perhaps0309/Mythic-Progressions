6037fc7c8132ef7b051c69563d9f7ad89e3e7b7c
mason26611 <masong1508@gmail.com>
2023-05-24 21:18:06 -0400

    Add bounty hunter file

--- src/main/java/perhaps/progressions/enchantments/enchantments/BountyHunter.java
+++ src/main/java/perhaps/progressions/enchantments/enchantments/BountyHunter.java
@@ -1,20 +1,11 @@
 package perhaps.progressions.enchantments.enchantments;
 
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.material.Material;
-import net.minecraftforge.event.entity.living.LivingExperienceDropEvent;
-import net.minecraftforge.event.world.BlockEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import perhaps.progressions.MythicProgressions;
 import perhaps.progressions.enchantments.EnchantmentRarity;
-import perhaps.progressions.enchantments.Enchantments;
 
 @Mod.EventBusSubscriber(modid = MythicProgressions.MOD_ID)
 public class BountyHunter extends Enchantment {
